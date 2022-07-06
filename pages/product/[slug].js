ed29f7a2de923a48b005f877fcd1797d2711e527
samantha1689 <samantha.lm.lee@gmail.com>
2022-07-06 12:20:40 -0700

    Initial commit 2022-07-06

---
+++ pages/product/[slug].js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const ProductDetails = ({ product, products }) => {
+	const { image, name, details, price } = product;
+
+	return (
+		<div>
+			<div className="product-detail-container">
+				<div>
+					<div className="image-container">
+						<img src="" />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export const getStaticProps = async ({ params: { slug } }) => {
+	const query = `*[_type == "product" && slug.current == '${slug}][0]`;
+	const productsQuery = '*[_type == "product"]';
+
+	const product = await client.fetch(query);
+	const products = await client.fetch(productsQuery);
+
+	return {
+		props: { product, products },
+	};
+};
+
+export default ProductDetails;
