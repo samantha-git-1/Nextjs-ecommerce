0086cb520196a8bb876818621daa25e38d0a120e
samantha1689 <samantha.lm.lee@gmail.com>
2022-07-11 15:07:59 -0700

    '7/11/2022'

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -70,7 +70,7 @@ const ProductDetails = ({ product, products }) => {
 			<div className="maylike-products-wrapper">
 				<h2>You may also like</h2>
 				<div className="marquee">
-					<div className="maylike-products-container track">
+					<div className="maylike-products-container track:hover">
 						{products.map((item) => (
 							<Product key={item.id} product={item} />
 						))}
